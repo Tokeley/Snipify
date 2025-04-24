0ecd0f7c50c9ac5ef1fa381aa368f21f78a26dfa
Alex Manning <alex.t.manning@gmail.com>
2025-04-24 15:29:50 +1200

    More prod fixes

--- snipify/src/components/MobileWrapper.jsx
+++ snipify/src/components/MobileWrapper.jsx
@@ -6,7 +6,7 @@ const MobileWrapper = ({ children }) => {
                 backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke-width='2' stroke='rgba(212, 212, 212, 0.3)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
             }}
         >
-            <div className="min-h-screen w-full px-4 py-10 overflow-y-auto  flex flex-col items-center justify-center">
+            <div className="min-h-screen w-full mx-2 px-4 py-10 overflow-y-auto  flex flex-col items-center justify-center">
                 {children}
             </div>
         </div>
