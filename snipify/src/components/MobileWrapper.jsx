734d3bcac4c4717f1b3662e30dc9e1c958606b4f
Alex Manning <alex.t.manning@gmail.com>
2025-04-24 16:05:27 +1200

    Styling fix 10

--- snipify/src/components/MobileWrapper.jsx
+++ snipify/src/components/MobileWrapper.jsx
@@ -1,7 +1,7 @@
 const MobileWrapper = ({ children }) => {
     return (
         <div 
-            className="w-full h-full place-items-center bg-neutral-100 overflow-x-hidden"
+            className="w-full min-h-screen place-items-center bg-neutral-100 overflow-x-hidden"
             style={{
                 backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke-width='2' stroke='rgba(212, 212, 212, 0.3)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
             }}
