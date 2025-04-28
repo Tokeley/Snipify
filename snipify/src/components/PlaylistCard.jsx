872e36e9bab11d74e32b13b5b431284aa683f8ed
Alex Manning <alex.t.manning@gmail.com>
2025-04-28 16:00:35 +1200

    Styling fix

--- snipify/src/components/PlaylistCard.jsx
+++ snipify/src/components/PlaylistCard.jsx
@@ -9,15 +9,15 @@ const PlaylistCard = ({ playlist }) => {
 
   return (
     <div
-      className="card card-side bg-base-100 shadow-md mb-4 w-full sm:w-72 md:w-80 lg:w-96 hover:cursor-pointer hover:bg-base-200 transition duration-300 ease-in-out"
+      className="card card-side bg-base-100 shadow-md mb-4 max-w-md hover:cursor-pointer hover:bg-base-200 transition duration-300 ease-in-out overflow-hidden"
       onClick={() => window.location.href = `/swipe-remove/${id}`}
     >
-      <figure className="w-28 h-28">
+      <figure className="w-28 h-28 flex-shrink-0">
         <img src={imageUrl} alt={name} className="object-cover w-full h-full" />
       </figure>
-      <div className="card-body p-4 overflow-hidden">
-        <h2 className="card-title text-base font-semibold leading-tight truncate w-full">{name}</h2>
-        <p className="text-sm text-gray-400 truncate">by {owner}</p>
+      <div className="card-body p-4 overflow-hidden ">
+        <h2 className="card-title text-base font-semibold leading-tight truncate w-full overflow-hidden whitespace-nowrap ">{name}</h2>
+        <p className="text-sm text-gray-400 truncate overflow-hidden whitespace-nowrap ">by {owner}</p>
         <p className="text-sm">{totalTracks} tracks</p>
       </div>
     </div>
