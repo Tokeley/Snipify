2779bf87bdbbbcab82ff6e80113461163cb9bd86
Alex Manning <alex.t.manning@gmail.com>
2025-04-28 17:05:06 +1200

    Width of playlist card fix

--- snipify/src/components/PlaylistCard.jsx
+++ snipify/src/components/PlaylistCard.jsx
@@ -9,7 +9,7 @@ const PlaylistCard = ({ playlist }) => {
 
   return (
     <div
-      className="card card-side bg-base-100 shadow-md mb-4 max-w-md hover:cursor-pointer hover:bg-base-200 transition duration-300 ease-in-out overflow-hidden"
+      className="card card-side bg-base-100 shadow-md mb-4 max-w-md w-80 hover:cursor-pointer hover:bg-base-200 transition duration-300 ease-in-out overflow-hidden"
       onClick={() => window.location.href = `/swipe-remove/${id}`}
     >
       <figure className="w-28 h-28 flex-shrink-0">
