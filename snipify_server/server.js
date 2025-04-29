ebf5bc063a43bf10bf89397baeee925bd3df1085
Alex Manning <alex.t.manning@gmail.com>
2025-04-29 13:51:21 +1200

    Another attempt

--- snipify_server/server.js
+++ snipify_server/server.js
@@ -34,7 +34,7 @@ app.use(session({
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-    domain: process.env.NODE_ENV === 'production' ? '.up.railway.app' : undefined, // <-- ADD THIS
+    domain: process.env.NODE_ENV === 'production' ? 'snipify-production.up.railway.app' : undefined, // <-- ADD THIS
   }
 }));
 
