a62974cda71b2d713df0a13e3543437f99572cb8
Alex Manning <alex.t.manning@gmail.com>
2025-04-30 13:50:04 +1200

    Linked search icon to search page

--- snipify/src/components/Navbar.jsx
+++ snipify/src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm sticky top-0 z-50 backdrop-blur-md">
       <div className="navbar-start">
-        <div >
+        <div>
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle ml-1">
-            <a  href="/settings" className="w-5 h-5 flex items-center justify-center">
+            <a href="/settings" className="w-5 h-5 flex items-center justify-center">
               <GearIcon />
             </a>
           </div>
@@ -24,12 +24,14 @@ const Navbar = () => {
       </div>
 
       <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle">
+        <button
+          className="btn btn-ghost btn-circle"
+          onClick={() => { window.location.href = '/from-playlist-or-album-select' }}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>
         </button>
-
       </div>
     </div>
   )
