5ff957114743603318b8ddf474ee1c053f1f165e
Alex Manning <alex.t.manning@gmail.com>
2025-05-01 16:10:10 +1200

    Remove search icon

--- snipify/src/components/Navbar.jsx
+++ snipify/src/components/Navbar.jsx
@@ -24,14 +24,7 @@ const Navbar = () => {
       </div>
 
       <div className="navbar-end">
-        <button
-          className="btn btn-ghost btn-circle"
-          onClick={() => { window.location.href = '/from-playlist-or-album-select' }}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-          </svg>
-        </button>
+        
       </div>
     </div>
   )
